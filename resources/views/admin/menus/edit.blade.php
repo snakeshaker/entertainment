b3a96bd3fc5331f6205914e0b81dc81f2f730b63
adilbek <serikbayev.a@nsk.kz>
2022-05-03 17:41:02 +0600

    Edit/Delete Menu created

--- resources/views/admin/menus/edit.blade.php
+++ resources/views/admin/menus/edit.blade.php
@@ -14,16 +14,20 @@
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form enctype="multipart/form-data" method="POST" action="{{ route('admin.menus.store') }}">
+                    <form enctype="multipart/form-data" method="POST" action="{{ route('admin.menus.update', $menu->id) }}">
                         @csrf
+                        @method('PUT')
                         <div class="sm:col-span-6">
                             <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                             <div class="mt-1">
-                                <input type="text" id="name" name="name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                <input value="{{ $menu->name }}" type="text" id="name" name="name" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
                         <div class="sm:col-span-6">
                             <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
+                            <div>
+                                <img src="{{ \Illuminate\Support\Facades\Storage::url($menu->image) }}" alt="Image" class="w-32 h-32">
+                            </div>
                             <div class="mt-1">
                                 <input type="file" id="image" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
@@ -31,29 +35,31 @@
                         <div class="sm:col-span-6 pt-5">
                             <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
                             <div class="mt-1">
-                                <input type="number" id="price" min="0.00" max="10000.00" step="0.01" name="price"
+                                <input value="{{ $menu->price }}" type="number" id="price" min="0.00" max="10000.00" step="0.01" name="price"
                                        class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                         </div>
                         <div class="sm:col-span-6 pt-5">
                             <label for="description" class="block text-sm font-medium text-gray-700">Descripton</label>
                             <div class="mt-1">
-                                <textarea id="description" rows="3" name="description" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                                <textarea id="description" rows="3" name="description" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                                    {{ $menu->description }}
+                                </textarea>
                             </div>
                         </div>
                         <div class="sm:col-span-6 pt-5">
-                            <label for="categories" class="block text-sm font-medium text-gray-700">Categories</label>
+                            <label for="body" class="block text-sm font-medium text-gray-700">Categories</label>
                             <div class="mt-1">
-                                <select multiple id="categories" name="categories" class="form-multiselect block w-full mt-1">
+                                <select multiple id="categories" name="catagories" class="form-multiselect block w-full mt-1">
                                     @foreach($categories as $category)
-                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        <option value="{{ $category->id }}" @selected($menu->categories->contains($category))>{{ $category->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
                         </div>
                         <div class="mt-6 p-4">
                             <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">
-                                Store
+                                Update
                             </button>
                         </div>
                     </form>
