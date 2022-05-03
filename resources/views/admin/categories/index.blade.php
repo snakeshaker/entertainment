3bf0f09fa784811f472fd507b34d3ab36ce283f3
adilbek <serikbayev.a@nsk.kz>
2022-05-03 15:34:51 +0600

    Edit/Delete Category created

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -42,6 +42,21 @@
                             <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
                                 {{ $category->description }}
                             </td>
+                            <td class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <div class="flex space-x-2">
+                                    <a href="{{ route('admin.categories.edit', $category->id) }}" class="px-4 py-2 bg-green-500 hover:bg-green-700 rounded-lg text-white">
+                                        Edit
+                                    </a>
+                                    <form class="px-4 py-2 bg-red-500 hover:bg-red-700 rounded-lg text-white"
+                                          method="POST"
+                                          action="{{ route('admin.categories.destroy', $category->id) }}"
+                                          onsubmit="return confirm('Are you sure?')">
+                                        @csrf
+                                        @method("DELETE")
+                                        <button type="submit">Delete</button>
+                                    </form>
+                                </div>
+                            </td>
                         </tr>
                     @endforeach
                     </tbody>
