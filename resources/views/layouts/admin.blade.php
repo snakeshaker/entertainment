7fa81865e2696ccfc291d3e8c684c34d69b33f84
adilbek <serikbayev.a@nsk.kz>
2022-05-05 11:28:30 +0600

    Added button to admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -42,6 +42,9 @@
                     <x-admin-nav-link :href="route('admin.reservations.index')" :active="request()->routeIs('admin.reservations.index')">
                         {{ __('Бронирования') }}
                     </x-admin-nav-link>
+                    <x-admin-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
+                        {{ __('На главную') }}
+                    </x-admin-nav-link>
                     <div @click.away="open = false" class="relative" x-data="{ open: false }">
                         <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark:bg-transparent dark:focus:text-white dark:hover:text-white dark:focus:bg-gray-600 dark:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
                             <span>{{ Auth::user()->name }}</span>
